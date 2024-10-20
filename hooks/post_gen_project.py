b636d564d2619f1dd75dce260673c0adf5c7f5d8
Anonymate <leng.0054@gmail.com>
2024-10-19 21:36:47 -0600

    Corrections

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,15 +1,12 @@
 import fs
 
-print("Running")
 CURRENT_DIR = fs.open_fs(".")
-print(CURRENT_DIR)
 PYSPARK_DIR = CURRENT_DIR.getsyspath("notebooks/PySpark")
-print(PYSPARK_DIR)
 
 project_packages = "{{ cookiecutter.project_packages }}"
 
 if project_packages != "BigData":
     try:
         CURRENT_DIR.removetree(PYSPARK_DIR)
-    except fs.errors.ResourceNotFound:
-        print("Fail to remove directory")
+    except Exception as e: 
+        print("Fail to remove directory", e)
